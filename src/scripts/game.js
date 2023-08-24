9c03ef869f4da49a6965011eca1a9c08943cbcdf
Joxiaren <2000jox@gmail.com>
2023-08-24 21:30:56 +0200

    add object hierarchy placeholder

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -2,11 +2,13 @@ import DOMManager from "./DOMManager";
 const DOMM = DOMManager.getManager();
 
 export default class Game{
-    constructor(){
+    constructor(players){
+        this.players = players;
         this.DOMElement = undefined;
     }
     setDOMElement(){
         if(this.DOMElement !== undefined) return;
         this.DOMElement = DOMM.createDOM('div', 'game');
     }
+
 }
--- src/scripts/player.js
+++ src/scripts/player.js
@@ -2,8 +2,9 @@ import DOMManager from "./DOMManager";
 const DOMM = DOMManager.getManager();
 
 export default class Player{
-    constructor(){
+    constructor(gameboard){
         this.DOMElement = undefined;
+        this.gameboard = gameboard;
     }
     setDOMElement(){
         if(this.DOMElement !== undefined) return;
